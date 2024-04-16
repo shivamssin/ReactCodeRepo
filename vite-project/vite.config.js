77b43c03177a758cd8caaaf7011e2d537c922871
shivamssin <36474322+shivamssin@users.noreply.github.com>
2024-04-17 04:37:02 +0530

    404 fix

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ReactCodeRepo/"
+  base: "/ReactCodeRepo/vite-project"
   build: {
     outDir: "./docs",
   }
